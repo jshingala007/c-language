2d2f0ca9e2d9a4cb96082c2aa6573617ff355251
jay-shingala <163421924+jshingala007@users.noreply.github.com>
2024-04-25 18:15:18 +0530

    Add files via upload

---
+++ 1D Array/p-2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ main(){
+	int start,end,i;
+	
+	printf("Enter first year:-");
+	scanf("%d",&start);
+	
+	printf("Enter Last year:-");
+	scanf("%d",&end);
+	
+	int arr[i];
+	
+	for(arr[i]=start; arr[i]<=end; arr[i]++){
+		if(arr[i] % 4== 0){
+			printf("%d ",arr[i]);
+		}
+	}
+}
